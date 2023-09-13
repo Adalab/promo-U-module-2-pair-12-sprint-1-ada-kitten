2f48e4162a97bd99c8ba9238e6564527ff1b992c
cristinanico <135840337+cristinanico@users.noreply.github.com>
2023-09-13 11:21:59 +0200

    2.6 Funciones

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 'use strict';
-
+//QUERYSELECTORS
 const listElement = document.querySelector('.js-list');
 
 const inputSearch = document.querySelector('.js_in_search_desc');
 
-const descrSearch = inputSearch.value;
+const btnSearch = document.querySelector('.js-btn-search');
 
 const urlKittenOne = 'https://dev.adalab.es/gato-siames.webp';
 
@@ -46,41 +46,7 @@ const btnCancel = document.querySelector('.js-btn-cancel');
 const newForm = document.querySelector('.js-new-form');
 const plusCircle = document.querySelector('.js-plus-circle');
 
-if (raceKittenOne === '') {
-  race = `Uy que despiste, no sabemos su raza`;
-} else {
-  race = raceKittenOne;
-}
-
-if (raceKittenTwo === '') {
-  race = `Uy que despiste, no sabemos su raza`;
-} else {
-  race = raceKittenTwo;
-}
-
-if (raceKittenThree === '') {
-  race = `Uy que despiste, no sabemos su raza`;
-} else {
-  race = raceKittenThree;
-}
-
-function renderKitten(url, desc, name, race) {
-  const kitten = `<li class="card">
-  <article>
-    <img
-      class="card_img"
-      src="${url}"
-      alt="siames-cat"
-    />
-    <h3 class="card_title">${name.toUpperCase()}</h3>
-    <h4 class="card_race">${race}</h4>
-    <p class="card_description">
-      ${desc}
-    </p>
-  </article>
-  </li>`;
-  return kitten;
-}
+//VARIABLES
 
 const kittenOne = renderKitten(
   urlKittenOne,
@@ -103,18 +69,34 @@ const kittenThree = renderKitten(
   raceKittenThree
 );
 
-/* listElement.innerHTML = kittenOne + kittenTwo + kittenThree; */
+//FUNCIONES
 
-if (descriptionKittenOne.includes(descrSearch)) {
-  listElement.innerHTML = kittenOne;
+function renderRace (race) {
+  if (race === '') {
+   return `Uy que despiste, no sabemos su raza`;
+  } else {
+  return race;
+  }
 }
 
-if (descriptionKittenTwo.includes(descrSearch)) {
-  /*   listElement.innerHTML = listElement.innerHTML + kittenTwo; */
-  listElement.innerHTML += kittenTwo;
-}
-if (descriptionKittenThree.includes(descrSearch)) {
-  listElement.innerHTML += kittenThree;
+// BONUS UNDEFINED
+
+function renderKitten(url, desc, name, race) {
+  const kitten = `<li class="card">
+  <article>
+    <img
+      class="card_img" 
+      src="${url}"
+      alt="siames-cat"
+    />
+    <h3 class="card_title">${name.toUpperCase()}</h3>
+    <h4 class="card_race">${renderRace(race)}</h4>
+    <p class="card_description">
+      ${desc}
+    </p>
+  </article>
+  </li>`;
+  return kitten;
 }
 
 function addNewKitten(event) {
@@ -124,24 +106,11 @@ function addNewKitten(event) {
   const valueName = inputName.value;
 
   if (valueDesc === '' || valuePhoto === '' || valueName === '') {
-    //completa el código
     labelMessageError.innerHTML = '¡Uy! parece que has olvidado algo';
     labelMessageError.classList.add('error-form');
   } else {
-    //completa el código
   }
 }
-
-btnAdd.addEventListener('click', addNewKitten);
-
-btnCancel.addEventListener('click', (event) => {
-  event.preventDefault();
-  inputDesc.value = '';
-  inputName.value = '';
-  inputPhoto.value = '';
-  labelMessageError.innerHTML = '';
-  hideNewCatForm();
-});
 
 function showNewCatForm() {
   newForm.classList.remove('collapsed');
@@ -159,4 +128,44 @@ function handleClickNewCatForm(event) {
   }
 }
 
+const filterKitten = (event) => {
+  event.preventDefault();
+  deleteList ();
+  addKittens ();
+};
+
+function addKittens () {
+  const descrSearch = inputSearch.value;
+  if (descriptionKittenOne.includes(descrSearch)) {
+    listElement.innerHTML += kittenOne;
+  }
+  if (descriptionKittenTwo.includes(descrSearch)) {
+    listElement.innerHTML += kittenTwo;
+  }
+  if (descriptionKittenThree.includes(descrSearch)) {
+    listElement.innerHTML += kittenThree;
+  }
+}
+
+function deleteList () {
+  listElement.innerHTML ='';
+}
+
+//EVENTOS
+
+btnAdd.addEventListener('click', addNewKitten);
+
+btnCancel.addEventListener('click', (event) => {
+  event.preventDefault();
+  inputDesc.value = '';
+  inputName.value = '';
+  inputPhoto.value = '';
+  labelMessageError.innerHTML = '';
+  hideNewCatForm();
+});
+
 plusCircle.addEventListener('click', handleClickNewCatForm);
+
+btnSearch.addEventListener ('click', filterKitten);
+
+//const descrSearch = inputSearch.value;
